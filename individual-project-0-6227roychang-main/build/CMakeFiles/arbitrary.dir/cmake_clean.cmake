ad45f5663211a5bdd3f29c1cc2482ec298b5f64c
6227roychang <tunweich@usc.edu>
2025-09-04 14:28:08 -0700

    with build

---
+++ individual-project-0-6227roychang-main/build/CMakeFiles/arbitrary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/arbitrary.dir/arbitrary.cpp.o"
+  "arbitrary.pdb"
+  "arbitrary"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/arbitrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
